edaa3bbd91cb23b314ca9aa2f9f0d767a80792c1
Vadim Cherepenichev <vadim.cvy@gmail.com>
2024-03-22 12:52:25 +0200

    Add todo

--- template-parts/dashboard/settings-pages/global/popup/field-settings_pt.php
+++ template-parts/dashboard/settings-pages/global/popup/field-settings_pt.php
@@ -18,4 +18,6 @@
   {
     require jab_resolve_path( __DIR__ . "/field-setting_$field_setting_name.php" );
   } ?>
+
+  // todo: add ability to choose if users should be able to select specific post or specific term only (post will be selected automatically by php on submission). The same shuold work for main field. So filter will show either specific posts or only specific terms.
 </div>
